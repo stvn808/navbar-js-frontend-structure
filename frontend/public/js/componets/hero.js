e4da1e80e0950769ecf4ed493d8370c145004c2f
Stiven Agudelo <stvnagudelo82@gmail.com>
2025-07-07 20:22:20 -0500

    feat: se agrego el hero, se mejoraron los estilos del navbar

---
+++ frontend/public/js/componets/hero.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const heroElement = document.querySelector(".hero");
+
+    if (heroElement) {
+        fetch("/frontend/public/views/components/hero.html")
+            .then(response => response.text())
+            .then(data => {
+                heroElement.innerHTML = data;
+            })
+            .catch(error => console.log("Error cargando el hero", error));
+    }   
+});
--- frontend/public/js/componets/navbar.js
+++ frontend/public/js/componets/navbar.js
@@ -1,10 +1,31 @@
 document.addEventListener("DOMContentLoaded", function(){
-    const navbarElement = document.querySelector(".navbar-container");
+    const navbarElement = document.querySelector(".navbar");
 
     if(navbarElement){
         fetch("/frontend/public/views/components/navbar.html")
-        .then(Response => Response.text())
-        .then(data => {navbarElement.innerHTML = data;
+        .then(response => response.text())
+        .then(data => {
+            navbarElement.innerHTML = data;
+
+            //logica para resaltar el enlace activo en navbar
+
+            //obtener la ruta actual 
+            //si no hay un archivo especifico se asume que es el index
+            const currentPage = window.location.pathname.split("/").pop() || "index.html";
+
+
+            //selcciona todos los enlaces del navbar que uaran la clase personalizada
+            const navLinks = navbarElement.querySelectorAll(".navbar__link"); 
+
+            //recorrer cada enlace del navbar 
+            navLinks.forEach(link => {
+
+                //verifica que el href del enlace incluye nombre de la pagina actual
+                if(link.getAttribute("href").includes(currentPage)){
+                    //si es la pagina actual se le asigna la clase 'active' para destacarla visualmete
+                    link.classList.add ("active"); 
+            }
+        });
     })
     .catch(error => console.error("error cargando el navbar", error));
 
